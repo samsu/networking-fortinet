4ad9d8b594a3cabe3f4e38db8cd78433e1674548
samsu <susltd.su@gmail.com>
2016-09-23 16:54:34 -0700

    test

--- networking_fortinet/agent/l2/openvswitch/ovs_neutron_agent.py
+++ networking_fortinet/agent/l2/openvswitch/ovs_neutron_agent.py
@@ -1321,14 +1321,24 @@ class OVSNeutronAgent(sg_rpc.SecurityGroupAgentRpcCallbackMixin,
         port_tags = self.int_br.get_port_tag_dict()
         import ipdb;ipdb.set_trace()
         changed_ports = set()
+        changed = False
         for lvm in self.local_vlan_map.values():
             for port in lvm.vif_ports.values():
-                if (
-                    port.port_name in port_tags
-                    and (port_tags[port.port_name] != lvm.vlan
-                         or (isinstance(port_tags[port.port_name], list)
-                             and lvm.vlan not in port_tags[port.port_name]))
-                ):
+                #if (port.port_name in port_tags
+                #    and port_tags[port.port_name] != lvm.vlan
+                #        and (isinstance(port_tags[port.port_name], list)
+                #             and lvm.vlan not in port_tags[port.port_name])):
+                if port.port_name in port_tags:
+                    if port.port_name in consts.FTNT_PORTS:
+                        if isinstance(port_tags[port.port_name], list):
+                            if lvm.vlan not in port_tags[port.port_name]:
+                                changed = True
+                        elif port_tags[port.port_name] != lvm.vlan:
+                            changed = True
+                    else:
+                        if port_tags[port.port_name] != lvm.vlan:
+                            changed = True
+                if changed:
                     LOG.info(
                         _LI("Port '%(port_name)s' has lost "
                             "its vlan tag '%(vlan_tag)d'!"),
