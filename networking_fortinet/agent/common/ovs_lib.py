8e42fbef141bfb674b8b870279df6fd916a61ac1
samsu <susltd.su@gmail.com>
2016-09-26 16:21:24 -0700

    test

--- networking_fortinet/agent/common/ovs_lib.py
+++ networking_fortinet/agent/common/ovs_lib.py
@@ -97,7 +97,7 @@ class FortinetOVSBridge(ovs_lib.OVSBridge):
 
         new_attrs = copy.deepcopy(cur_attrs[0])
         added_attrs = dict(interface_attr_tuples)
-        for col, attr in added_attrs.values():
+        for col, attr in added_attrs.iteritems():
             # k is the column name, v is the column value
             import ipdb;ipdb.set_trace()
             new_attr = new_attrs[col]
