42ee9755ae54640548ea6102285259f548a70456
samsu <susltd.su@gmail.com>
2016-09-29 16:28:20 -0700

    test

--- networking_fortinet/agent/l2/openvswitch/ovs_neutron_agent.py
+++ networking_fortinet/agent/l2/openvswitch/ovs_neutron_agent.py
@@ -359,7 +359,6 @@ class OVSNeutronAgent(sg_rpc.SecurityGroupAgentRpcCallbackMixin,
             net_uuid = local_vlan_map.get('net_uuid')
             if (net_uuid and net_uuid not in self._local_vlan_hints
                 and local_vlan != DEAD_VLAN_TAG):
-                import ipdb;ipdb.set_trace()
                 ## samsu: use discard to replace the remove temporarily, here
                 ## need to rewrite and handle 'other_config' info later.
                 self.available_local_vlans.discard(local_vlan)
