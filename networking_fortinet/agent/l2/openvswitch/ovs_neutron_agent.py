09791b5c999c7b7a2838c099ed54d3686feff3cd
samsu <susltd.su@gmail.com>
2016-09-30 13:59:43 -0700

    test

--- networking_fortinet/agent/l2/openvswitch/ovs_neutron_agent.py
+++ networking_fortinet/agent/l2/openvswitch/ovs_neutron_agent.py
@@ -340,12 +340,12 @@ class OVSNeutronAgent(sg_rpc.SecurityGroupAgentRpcCallbackMixin,
     @log_helpers.log_method_call
     def _restore_local_vlan_map(self):
         self._local_vlan_hints = {}
+        import ipdb;ipdb.set_trace()
         cur_ports = self.int_br.get_vif_ports()
         port_names = [p.port_name for p in cur_ports]
         port_info = self.int_br.get_ports_attributes(
             "Port", columns=["name", "other_config", "tag"], ports=port_names)
         by_name = {x['name']: x for x in port_info}
-        import ipdb;ipdb.set_trace()
         for port in cur_ports:
             # if a port was deleted between get_vif_ports and
             # get_ports_attributes, we
