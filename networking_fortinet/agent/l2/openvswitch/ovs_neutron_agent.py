c558c47fbef2a59ed55fb3b950b3226fb409a527
samsu <susltd.su@gmail.com>
2016-10-12 16:44:09 -0700

    test

--- networking_fortinet/agent/l2/openvswitch/ovs_neutron_agent.py
+++ networking_fortinet/agent/l2/openvswitch/ovs_neutron_agent.py
@@ -1310,7 +1310,6 @@ class OVSNeutronAgent(sg_rpc.SecurityGroupAgentRpcCallbackMixin,
     @log_helpers.log_method_call
     def scan_ports(self, registered_ports, sync, updated_ports=None):
         print "@@@## self.int_br.get_vif_port_set=", self.int_br.get_vif_port_set
-        import ipdb;ipdb.set_trace()
         cur_ports = self.int_br.get_vif_port_set()
         self.int_br_device_count = len(cur_ports)
         port_info = self._get_port_info(registered_ports, cur_ports, sync)
